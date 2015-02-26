df8b8185b4efb69ea2755e634295c833ca5c0791
Tejaswi Gowda <ec2-user@ip-10-176-161-26.us-west-1.compute.internal>
2015-02-26 19:49:01 +0000

    ac

--- nodeserver.js
+++ nodeserver.js
@@ -1,4 +1,23 @@
-var express = require("express"),
+
+  var nodemailer = require("nodemailer");
+
+  // create reusable transport method (opens pool of SMTP connections)
+   var transporter = nodemailer.createTransport("SMTP",{
+       service: "Gmail",
+        auth: {
+           user: "noreply@foxyninjastudios.com",
+           pass: "HasNothing11"
+        }
+    });
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: "FNS No-reply<noreply@foxyninjastudios.com>", // sender address
+    to: "",
+    subject: "CONTACT: foxyninjastudios.com", // Subject line
+    text: '',
+    html: ""
+  }
+  var express = require("express"),
         app = express(),
             bodyParser = require('body-parser'),
                 errorHandler = require('errorhandler'),
@@ -9,6 +28,15 @@ var express = require("express"),
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
+});
+
+app.get("/handleContact", function (req, res) {
+	mailOptions.to = "contact@foxyninjastudios.com"
+	mailOptions.subject = "Contact : foxyninjastudios.com";
+	mailOptions.text = 
+                        "\n"+
+	transport.sendMail(mailOptions, function(error, response){console.log(error)});
+    res.end("1");
 });
 
 app.use(methodOverride());
